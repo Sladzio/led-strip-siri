a2e9704e07fbf5899223bdda5faffb11b774b7c9
EC2 Default User <ec2-user@ip-172-31-37-151.us-east-2.compute.internal>
2018-12-19 21:34:06 +0000

    Something

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ const exe = require('child_process').exec;
 
 app.get('/',function(req,res) {
     console.log(__dirname);
-  res.send("Hello World");
+  res.send("House Automation Server is running!");
 });
 
 app.get('/status',function(req,res) {
   var cmd = 'sudo ' + __dirname + '/rf24-RGB-remote ' + 1 + ' '+ 0;
   exe( cmd ,(error, stdout, stderr) => {
-     if (error) {
+     if (error)
+     {
       throw error;
      }
         res.send(stdout);
      });
 });
 
-app.listen(3000);
+app.listen(8080);
